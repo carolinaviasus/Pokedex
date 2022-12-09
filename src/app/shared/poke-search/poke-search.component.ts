dc065a479268ca4a1a9959d80a62e7b0fea1d9d3
carolinaviasus <carolinaviasus@outlook.com>
2022-12-09 13:08:42 -0500

    sea

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'poke-search',
   templateUrl: './poke-search.component.html',
   styleUrls: ['./poke-search.component.scss']
 })
 export class PokeSearchComponent {
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
   constructor(
     ) { }
     ngOnInit(): void {
