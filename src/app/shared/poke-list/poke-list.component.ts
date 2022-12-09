fe83c7893368b26ba79676a2ea54d577094c9b85
carolinaviasus <carolinaviasus@outlook.com>
2022-12-09 00:48:34 -0500

    cambio

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  private setAllPokemons: any;
+  public getAllPokemons: any;
   constructor(
   // private pokeApiService: PokeApiService
+  private PokeApiService: PokeApiService
+  
   ) { }
   ngOnInit(): void {
+    this.PokeApiService.apiListAllPokemons.subscribe(
+      res => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+        console.log(this.getAllPokemons)
+        // },
+      // error => {
+      //   this.apiError = true;
+      }
+    );
   }
   vermas(){
     console.log('ir a ver mas')
   }
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter( (res: any ) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
+  }
+
 }
